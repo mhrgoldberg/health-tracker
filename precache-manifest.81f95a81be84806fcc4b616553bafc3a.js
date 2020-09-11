983a77a756966f2152cd319a01af1b0403370ff1
mitchellreiss <mitchellreiss@gmail.com>
2020-09-11 07:58:36 -0700

    Updates

---
+++ precache-manifest.81f95a81be84806fcc4b616553bafc3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4442e349b788bf15a8981f9f455d9340",
+    "url": "/health-tracker/index.html"
+  },
+  {
+    "revision": "1a23d5fb07bbbdd845ac",
+    "url": "/health-tracker/static/css/main.038d378c.chunk.css"
+  },
+  {
+    "revision": "5d724d69e7a3fa0f6039",
+    "url": "/health-tracker/static/js/2.dc5586ba.chunk.js"
+  },
+  {
+    "revision": "8b81bc38395cca664a526c4e304a6603",
+    "url": "/health-tracker/static/js/2.dc5586ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a23d5fb07bbbdd845ac",
+    "url": "/health-tracker/static/js/main.328df3a2.chunk.js"
+  },
+  {
+    "revision": "e3447dfed85beda0f16b",
+    "url": "/health-tracker/static/js/runtime-main.405f84bd.js"
+  },
+  {
+    "revision": "7e4dbd4c824e926df78c3276fd21adcd",
+    "url": "/health-tracker/static/media/human.7e4dbd4c.svg"
+  }
+]);
